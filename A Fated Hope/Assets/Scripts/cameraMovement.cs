82ed83cba241befdba3dc21e46e717c72aa11636
Zealous-Create <zacharycasteel4808@gmail.com>
2025-12-12 16:46:36 -0500

    Update cameraMovement.cs

--- A Fated Hope/Assets/Scripts/cameraMovement.cs
+++ A Fated Hope/Assets/Scripts/cameraMovement.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class cameraMovement : MonoBehaviour
 {
+
+    public float xSens;
+    public float ySens;
+    public Transform orentation;
+    
+    float xRotation;
+    float yrotation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSens;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySens;
+
+        xRotation += mouseY;
+        yrotation -= mouseX;
+
+        xRotation = Mathf.Clamp(mouseX,-90f,90f);
     }
 }
